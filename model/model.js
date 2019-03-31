370622042f54613cf3a74f4c9a98f7619cb3b11e
laxmanpole <polelaxman001@gmail.com>
2019-03-31 17:47:29 +0530

    frontend commit

--- model/model.js
+++ model/model.js
@@ -8,12 +8,14 @@ var userSchema = new mongoSchema({
     "email": { type: String, required: [true, "Email is required"] },
     "password": { type: String, required: [true, "password is required"] },
 }, {
-        timestamps: true
-    });
+    timestamps: true
+});
+
 function usermodel() {
 
 }
 var user = mongoose.model('user', userSchema);
+
 function hash(password) {
     var salt = bcrypt.genSaltSync(10);
     var hashPassword = bcrypt.hashSync(password, salt);
@@ -29,10 +31,9 @@ usermodel.prototype.register = (body, callback) => {
         } else if (data.length > 0) {
             response = { "error": true, "message": "Email already exists ", "errorCode": 404 };
             return callback(response);
-        }
-        else {
+        } else {
             const newUser = new user({
-                
+
                 "firstname": body.firstname,
                 "lastname": body.lastname,
                 "email": body.email,
@@ -49,7 +50,7 @@ usermodel.prototype.register = (body, callback) => {
                     console.log("registered successfully");
                     callback(null, result);
                     console.log("no return statements ..registered successfully");
-                    
+
                 }
             })
         }
@@ -61,7 +62,7 @@ usermodel.prototype.login = (body, callback) => {
         if (err) {
             return callback(err);
         } else if (data.length > 0) {
-            bcrypt.compare(body.password, data[0].password, (err, res)=> {
+            bcrypt.compare(body.password, data[0].password, (err, res) => {
                 if (err) {
                     return callback(err);
                 } else if (res) {
@@ -83,62 +84,60 @@ usermodel.prototype.login = (body, callback) => {
 }
 usermodel.prototype.forgotPassword = (body, callback) => {
     // console.log("body in model==>",body);
-     
-     user.find({ "email": body.email }, (err, data) => {
-         if (err) {
-             return callback(err);
-         } else if (data){
-             console.log("data in models==>",data[0]._id);
-             
-             //console.log(data)
- 
-             return callback(null, data)
-         }
-         else {
-             return callback("Invalid User ");
-         }
-     });
- }
- 
- usermodel.prototype.resetPassword = (body, callback) => {
-     user.find({ 'email': body.email }, (err, data) => {
-         if (err) {
-             console.log("Error in register user schema ");
-             return callback(err);
-         } else if (data.length > 0) {
-             response = { "error": true, "message": "Email already exists ", "errorCode": 404 };
-             return callback(response);
-         }
-         else {
-             const newUser = new user({
-                 'firstname': body.firstname,
-                 'lastname': body.lastname,
-                 'email': body.email,
-                 'password': hash(body.password)
-             });
-             newUser.save((err, result) => {
-                 if (err) {
-                     console.log("error in model file", err);
-                     return callback(err);
-                 } else {
-                     console.log("data save successfully", result);
-                     return callback(null, result);
-                 }
-             })
-         }
-     });
- 
- }
- 
- usermodel.prototype.getAllUser = (req,callback) => {
-     user.find({}, (err, data)=>{
-        if(err){
-            callback("error is in model" + err)
-        }else{
-            callback(null , data);
+
+    user.find({ "email": body.email }, (err, data) => {
+        if (err) {
+            return callback(err);
+        } else if (data) {
+            console.log("data in models==>", data[0]._id);
+
+            //console.log(data)
+
+            return callback(null, data)
+        } else {
+            return callback("Invalid User ");
         }
-     })
- } 
+    });
+}
+
+usermodel.prototype.resetPassword = (body, callback) => {
+    user.find({ 'email': body.email }, (err, data) => {
+        if (err) {
+            console.log("Error in register user schema ");
+            return callback(err);
+        } else if (data.length > 0) {
+            response = { "error": true, "message": "Email already exists ", "errorCode": 404 };
+            return callback(response);
+        } else {
+            const newUser = new user({
+                'firstname': body.firstname,
+                'lastname': body.lastname,
+                'email': body.email,
+                'password': hash(body.password)
+            });
+            newUser.save((err, result) => {
+                if (err) {
+                    console.log("error in model file", err);
+                    return callback(err);
+                } else {
+                    console.log("data save successfully", result);
+                    return callback(null, result);
+                }
+            })
+        }
+    });
+
+}
+
+usermodel.prototype.getAllUser = (req, callback) => {
+    user.find({}, (err, data) => {
+        if (err) {
+            callback("error is in model" + err)
+        } else {
+            callback(null, data);
+        }
+    })
+}
 
 
 
